22f910736da3b225f46840bc7ad1ec41fefddece
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2010-02-08 04:05:17 -0300

    delete test-isolinux.sh
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>

--- configs/syslinux-iso/test-isolinux.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p isolinux-test/boot
-cp -r boot-files/* isolinux-test/boot/
-cp /usr/lib/syslinux/isolinux.bin isolinux-test/boot/isolinux/
-cp /usr/lib/syslinux/*.c32 isolinux-test/boot/isolinux/
-mkisofs -b boot/isolinux/isolinux.bin -c boot/isolinux/boot.cat -r -l -uid 0 -gid 0 -udf -allow-limited-size -iso-level 3 -input-charset utf-8 -boot-load-size 4 -no-emul-boot -boot-info-table -o isolinux-test.iso isolinux-test && qemu-kvm -cdrom isolinux-test.iso
