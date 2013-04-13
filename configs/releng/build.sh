86e1f5057256d29490f26a7c2b576ef4f508bf62
Pierre Schmitz <pierre@archlinux.de>
2013-04-13 15:58:37 +0200

    [configs/releng] pacman 4.1 compatibility
    * Remove the unattended-keyring-init patch as this was applied by upstream with pacamn 4.1
    * Update our pacman.conf following the new upstream default
    
    Signed-off-by: Pierre Schmitz <pierre@archlinux.de>
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>

--- configs/releng/build.sh
+++ configs/releng/build.sh
@@ -79,7 +79,6 @@ make_setup_mkinitcpio() {
 make_customize_root_image() {
     cp -af ${script_path}/root-image ${work_dir}/${arch}
 
-    patch ${work_dir}/${arch}/root-image/usr/bin/pacman-key < ${script_path}/pacman-key-4.0.3_unattended-keyring-init.patch
     curl -o ${work_dir}/${arch}/root-image/etc/pacman.d/mirrorlist 'https://www.archlinux.org/mirrorlist/?country=all&protocol=http&use_mirror_status=on'
 
     lynx -dump -nolist 'https://wiki.archlinux.org/index.php/Installation_Guide?action=render' >> ${work_dir}/${arch}/root-image/root/install.txt
