5d75049c119983aad719d03e92557f10aef5d199
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2013-06-04 21:33:52 -0300

    [configs/releng] Update zsh path to match /etc/shells
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>

--- configs/releng/root-image/root/customize_root_image.sh
+++ configs/releng/root-image/root/customize_root_image.sh
@@ -7,10 +7,10 @@ locale-gen
 
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 
-usermod -s /bin/zsh root
+usermod -s /usr/bin/zsh root
 cp -aT /etc/skel/ /root/
 
-useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh arch
+useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /usr/bin/zsh arch
 
 chmod 750 /etc/sudoers.d
 chmod 440 /etc/sudoers.d/g_wheel
