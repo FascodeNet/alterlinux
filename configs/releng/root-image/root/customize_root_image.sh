c5539c799c6d001f5360f8bac94d7c4c0da712f2
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2013-09-21 16:53:45 -0300

    [configs/releng] explicit set 'Storage=volatile' for journald
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>

--- configs/releng/root-image/root/customize_root_image.sh
+++ configs/releng/root-image/root/customize_root_image.sh
@@ -16,5 +16,6 @@ chmod 750 /etc/sudoers.d
 chmod 440 /etc/sudoers.d/g_wheel
 
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
 systemctl enable multi-user.target pacman-init.service choose-mirror.service
