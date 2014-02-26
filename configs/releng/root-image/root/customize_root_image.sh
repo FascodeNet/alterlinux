6836b770a207e005834a26e08d2b139af20c8c0a
Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>
2014-02-25 21:20:48 -0300

    [configs/releng] Update the way to set default multi-user.target
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@yahoo.com.ar>

--- configs/releng/root-image/root/customize_root_image.sh
+++ configs/releng/root-image/root/customize_root_image.sh
@@ -18,4 +18,5 @@ chmod 440 /etc/sudoers.d/g_wheel
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
-systemctl enable multi-user.target pacman-init.service choose-mirror.service
+systemctl enable pacman-init.service choose-mirror.service
+systemctl set-default multi-user.target
