8e7601caae6ecc2af07563696281b9cb5038311c
Gerardo Exequiel Pozzi <vmlinuz386@gmail.com>
2015-08-11 21:45:33 -0300

    [configs/releng] Ignore Lid/Sus/Hib switch/keys by logind
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@gmail.com>

--- configs/releng/airootfs/root/customize_airootfs.sh
+++ configs/releng/airootfs/root/customize_airootfs.sh
@@ -15,5 +15,9 @@ sed -i 's/#\(PermitRootLogin \)no/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
+sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
+sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
+
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
