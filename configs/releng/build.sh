526be1579e515beb7bfb04b154b23450eb6589cb
Gerardo Exequiel Pozzi <vmlinuz386@gmail.com>
2016-03-01 21:51:50 -0300

    [archiso] Increase EFI image size for El Torito boot method
    Current build leaves ~800K free of 31M in the FAT filesystem, adding 9M.
    
    Going beyond 65535 sectors of 512-byte is a bit special,
    but works for EFI. Image size is reported a zero in boot catalog,
    but xorriso does a good job, and writes right value in hybrid-partition.
    
    Tested booting in qemu in both modes (cd-rom and hybrid) and works fine.
    
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@gmail.com>

--- configs/releng/build.sh
+++ configs/releng/build.sh
@@ -169,7 +169,7 @@ make_efi() {
 # Prepare efiboot.img::/EFI for "El Torito" EFI boot mode
 make_efiboot() {
     mkdir -p ${work_dir}/iso/EFI/archiso
-    truncate -s 31M ${work_dir}/iso/EFI/archiso/efiboot.img
+    truncate -s 40M ${work_dir}/iso/EFI/archiso/efiboot.img
     mkfs.vfat -n ARCHISO_EFI ${work_dir}/iso/EFI/archiso/efiboot.img
 
     mkdir -p ${work_dir}/efiboot
