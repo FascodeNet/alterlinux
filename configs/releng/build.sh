09e5693bab396ea578f8162b0be218fc90c21113
Gerardo Exequiel Pozzi <vmlinuz386@gmail.com>
2016-12-07 19:21:16 -0300

    [configs/releng] Increse efiboot.img again
    We reached the upper limit again [#1], increasing sufficiently
    to do not touch again in long time.
    
    [#1] 526be1579e515beb7bfb04b154b23450eb6589cb
    
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@gmail.com>

--- configs/releng/build.sh
+++ configs/releng/build.sh
@@ -169,7 +169,7 @@ make_efi() {
 # Prepare efiboot.img::/EFI for "El Torito" EFI boot mode
 make_efiboot() {
     mkdir -p ${work_dir}/iso/EFI/archiso
-    truncate -s 40M ${work_dir}/iso/EFI/archiso/efiboot.img
+    truncate -s 64M ${work_dir}/iso/EFI/archiso/efiboot.img
     mkfs.fat -n ARCHISO_EFI ${work_dir}/iso/EFI/archiso/efiboot.img
 
     mkdir -p ${work_dir}/efiboot
