52a00da605ca7e0e367de44bf7c52fab033af1ba
Gerardo Exequiel Pozzi <vmlinuz386@gmail.com>
2017-03-01 23:05:35 -0300

    [configs/releng] Oops, rename -dual.iso -> -x86_64.iso
    Signed-off-by: Gerardo Exequiel Pozzi <vmlinuz386@gmail.com>

--- configs/releng/build.sh
+++ configs/releng/build.sh
@@ -213,7 +213,7 @@ make_prepare() {
 
 # Build ISO
 make_iso() {
-    mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "${iso_name}-${iso_version}-dual.iso"
+    mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -o "${out_dir}" iso "${iso_name}-${iso_version}-x86_64.iso"
 }
 
 if [[ ${EUID} -ne 0 ]]; then
