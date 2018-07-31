ddad85c86aef056c5475139e13f771856b47d1f4
Eli Schwartz via arch-releng <arch-releng@archlinux.org>
2018-07-31 19:27:11 -0400

    make detection of kernel modules dir more robust
    extract `uname -r` from the contents of vmlinuz-linux rather than make
    assumptions about the kernel EXTRAVERSION.
    
    This is trivially customizable by changing the chosen kernel image.
    
    Fixes FS#59496
    
    Signed-off-by: Eli Schwartz <eschwartz@archlinux.org>

--- configs/releng/build.sh
+++ configs/releng/build.sh
@@ -124,6 +124,7 @@ make_boot_extra() {
 
 # Prepare /${install_dir}/boot/syslinux
 make_syslinux() {
+    _uname_r=$(file -b ${work_dir}/x86_64/airootfs/boot/vmlinuz-linux| awk 'f{print;f=0} /version/{f=1}' RS=' ')
     mkdir -p ${work_dir}/iso/${install_dir}/boot/syslinux
     for _cfg in ${script_path}/syslinux/*.cfg; do
         sed "s|%ARCHISO_LABEL%|${iso_label}|g;
@@ -135,7 +136,7 @@ make_syslinux() {
     cp ${work_dir}/x86_64/airootfs/usr/lib/syslinux/bios/memdisk ${work_dir}/iso/${install_dir}/boot/syslinux
     mkdir -p ${work_dir}/iso/${install_dir}/boot/syslinux/hdt
     gzip -c -9 ${work_dir}/x86_64/airootfs/usr/share/hwdata/pci.ids > ${work_dir}/iso/${install_dir}/boot/syslinux/hdt/pciids.gz
-    gzip -c -9 ${work_dir}/x86_64/airootfs/usr/lib/modules/*-ARCH/modules.alias > ${work_dir}/iso/${install_dir}/boot/syslinux/hdt/modalias.gz
+    gzip -c -9 ${work_dir}/x86_64/airootfs/usr/lib/modules/${_uname_r}/modules.alias > ${work_dir}/iso/${install_dir}/boot/syslinux/hdt/modalias.gz
 }
 
 # Prepare /isolinux
