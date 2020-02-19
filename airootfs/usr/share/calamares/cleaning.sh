8cf24a64a650ed4a790666d965ee8b8652e8ad72
hayao <shun819.mail@gmail.com>
2020-02-19 18:32:18 +0900

    [update] : Improved cleanup exception handling on exit.

--- airootfs/usr/share/calamares/cleaning.sh
+++ airootfs/usr/share/calamares/cleaning.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-rm -rf /etc/skel/Desktop
-rm /etc/systemd/system/getty@tty1.service.d/autologin.conf
-rm /root/.automated_script.sh
-rm /etc/mkinitcpio-archiso.conf
-rm -r /etc/initcpio
+function remove () {
+    local list
+    local file
+    local exit_code
+    list=($(echo "$@"))
+    for file in "${list[@]}"; do
+        if [[ -f ${file} ]]; then
+            rm -f "${file}"
+        elif [[ -d ${file} ]]; then
+            rm -rf "${file}"
+        fi
+    done
+}
+
+remove /etc/skel/Desktop
+remove /etc/systemd/system/getty@tty1.service.d/autologin.conf
+remove /root/.automated_script.sh
+remove /etc/mkinitcpio-archiso.conf
+remove /etc/initcpio
