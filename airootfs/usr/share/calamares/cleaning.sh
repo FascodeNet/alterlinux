dc3464ebb70ba9e6422925491c3c4963ae105395
hayao <shun819.mail@gmail.com>
2020-02-28 18:30:39 +0900

    [update] : Added bookmark.

--- airootfs/usr/share/calamares/cleaning.sh
+++ airootfs/usr/share/calamares/cleaning.sh
@@ -3,7 +3,6 @@
 function remove () {
     local list
     local file
-    local exit_code
     list=($(echo "$@"))
     for file in "${list[@]}"; do
         if [[ -f ${file} ]]; then
@@ -14,8 +13,28 @@ function remove () {
     done
 }
 
+while getopts 'u:' arg; do
+    case "${arg}" in
+        u) user="${OPTARG}";;
+    esac
+done
+
 remove /etc/skel/Desktop
+remove /etc/skel/.config/gtk-3.0/bookmarks
+remove /home/${user}/Desktop/calamares.desktop
+remove /home/${user}/.config/gtk-3.0/bookmarks
 remove /etc/systemd/system/getty@tty1.service.d/autologin.conf
 remove /root/.automated_script.sh
 remove /etc/mkinitcpio-archiso.conf
 remove /etc/initcpio
+
+cat > /etc/skel/.config/gtk-3.0/bookmarks << EOF
+file:///home/${user}/Pictures
+file:///home/${user}/etc/skel/.config/gtk-3.0/bookmarks/Videos
+file:///home/${user}/Templates
+file:///home/${user}/Music
+file:///home/${user}/Downloads
+file:///home/${user}/Documents
+EOF
+
+cp /etc/skel/.config/gtk-3.0/bookmarks /home/${user}/.config/gtk-3.0/bookmarks
