0d3158888cf316c5493baba0097fed6aa14e4e37
hayao <shun819.mail@gmail.com>
2020-03-20 13:11:07 +0900

    [update] : Added remove function.

--- channels/xfce/airootfs/root/customize_airootfs_xfce.sh
+++ channels/xfce/airootfs/root/customize_airootfs_xfce.sh
@@ -2,6 +2,22 @@
 
 set -e -u
 
+# Delete file only if file exists
+# remove <file1> <file2> ...
+function remove () {
+    local _list
+    local _file
+    _list=($(echo "$@"))
+    for _file in "${_list[@]}"; do
+        if [[ -f ${_file} ]]; then
+            rm -f "${_file}"
+        elif [[ -d ${_file} ]]; then
+            rm -rf "${_file}"
+        fi
+        echo "${_file} was deleted."
+    done
+}
+
 
 # Replace wallpaper.
 if [[ -f /usr/share/backgrounds/xfce/xfce-stripes.png ]]; then
