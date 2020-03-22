e9303b7ef7c12200f7e679790e0bea2172f2e7b5
hayao <shun819.mail@gmail.com>
2020-03-22 21:51:14 +0900

    [fix] : Don't include empty directories in channels.

--- build.sh
+++ build.sh
@@ -629,20 +629,41 @@ fi
 
 
 # Parse options
-set +u
+set +eu
+
 shift $((OPTIND - 1))
 
 if [[ -n "${1}" ]]; then
     channel_name="${1}"
+
+    # Channel list
+    # check_channel <channel name>
+    check_channel() {
+        local channel_list
+        local i
+        channel_list=()
+        for i in $(ls -l "${script_path}"/channels/ | awk '$1 ~ /d/ {print $9 }'); do
+            if [[ -n $(ls "${script_path}"/channels/${i}) ]] && [[ ! ${i} = "share" ]]; then
+                channel_list="${channel_list[@]} ${i}"
+            fi
+        done
+        for i in ${channel_list[@]}; do
+            if [[ ${i} = ${1} ]]; then
+                echo -n "true"
+                return 0
+            fi
+        done
+        echo -n "false"
+        return 1
+    }
+
+    if [[ $(check_channel ${channel_name}) = false ]]; then
+        echo "Invalid channel ${channel_name}" >&2
+        exit 1
+    fi
 fi
 
-if [[ -z $(ls -l "${script_path}"/channels/ | awk '$1 ~ /d/ {print $9 }' | grep -xv share | grep -x "${channel_name}") ]]; then
-    echo "Invalid channel '${channel_name}'" >&2
-    exit 1
-fi
-
-
-set -u
+set -eu
 
 
 # Create a working directory.
