d8667316092d2f0a078b2df6447f3909db41711b
hayao <shun819.mail@gmail.com>
2020-03-23 12:06:28 +0900

    [remove] : Removed code for xfce.

--- channels/plasma/airootfs/root/customize_airootfs_plasma.sh
+++ channels/plasma/airootfs/root/customize_airootfs_plasma.sh
@@ -17,11 +17,3 @@ function remove () {
         echo "${_file} was deleted."
     done
 }
-
-
-# Replace wallpaper.
-#if [[ -f /usr/share/backgrounds/xfce/xfce-stripes.png ]]; then
-#    remove /usr/share/backgrounds/xfce/xfce-stripes.png
-#    ln -s /usr/share/backgrounds/alter.png /usr/share/backgrounds/xfce/xfce-stripes.png
-#fi
-#[[ -f /usr/share/backgrounds/alter.png ]] && chmod 644 /usr/share/backgrounds/alter.png
