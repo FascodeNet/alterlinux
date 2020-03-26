240313804d52e6df86522b82a08421ae4ed8f8f7
hayao <shun819.mail@gmail.com>
2020-03-26 23:46:18 +0900

    [fix] : Fixed typo.

--- channels/xfce/airootfs/etc/skel/.setup.sh
+++ channels/xfce/airootfs/etc/skel/.setup.sh
@@ -5,13 +5,13 @@ touch ${HOME}/.config/gtk-3.0/bookmarks
 
 source ${HOME}/.config/user-dirs.dirs
 
-cat  >> "${HOME}/.config/gtk-3.0/bookmarks" << EOF
+cat > "${HOME}/.config/gtk-3.0/bookmarks" << EOF
 file://${XDG_DOCUMENTS_DIR}/
 file://${XDG_DOWNLOAD_DIR}/
 file://${XDG_MUSIC_DIR}/
 file://${XDG_PICTURES_DIR}
 file://${XDG_TEMPLATES_DIR}
 file://${XDG_VIDEOS_DIR}
-EOF 
+EOF
 
 rm -f ~/.setup.sh
