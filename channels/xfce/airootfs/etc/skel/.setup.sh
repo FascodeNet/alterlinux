93a963e9bc5ff851951b53c1eeff955225065730
hayao <shun819.mail@gmail.com>
2020-03-26 23:22:07 +0900

    [fix] : Fixed regetating bookmarks.

--- channels/xfce/airootfs/etc/skel/.setup.sh
+++ channels/xfce/airootfs/etc/skel/.setup.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-echo "file://${HOME}/Documents/\nfile://${HOME}/Downloads/\nfile://${HOME}/Music/\nfile://${HOME}/Pictures/\nfile://${HOME}/Templates/\nfile://${HOME}/Videos/">"${HOME}/.config/gtk-3.0/bookmarks"
+mkdir -p ${HOME}/.config/gtk-3.0/
+touch ${HOME}/.config/gtk-3.0/bookmarks
+
+source ${HOME}/.config/user-dirs.dirs
+
+cat  >> "${HOME}/.config/gtk-3.0/bookmarks" << EOF
+file://${XDG_DOCUMENTS_DIR}/
+file://${XDG_DOWNLOAD_DIR}/
+file://${XDG_MUSIC_DIR}/
+file://${XDG_PICTURES_DIR}
+file://${XDG_TEMPLATES_DIR}
+file://${XDG_VIDEOS_DIR}
+EOF 
 
 rm -f ~/.setup.sh
