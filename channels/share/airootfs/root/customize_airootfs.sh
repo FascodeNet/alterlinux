1a0d3add4594d9b7d90e396c376019929cded8ef
hayao <shun819.mail@gmail.com>
2020-04-07 14:24:05 +0900

    [update] : OS name can be changed in calamares

--- channels/share/airootfs/root/customize_airootfs.sh
+++ channels/share/airootfs/root/customize_airootfs.sh
@@ -85,6 +85,10 @@ else
 fi
 
 
+# Set os name
+sed -i "s/%OS_NAME%/${os_name}/g" /etc/skel/Desktop/calamares.desktop
+
+
 # If rebuild is enabled, do not create users.
 # This is described in detail on ArchWiki.
 if [[ ${rebuild} = false ]]; then
