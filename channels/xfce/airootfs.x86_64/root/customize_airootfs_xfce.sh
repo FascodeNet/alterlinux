3ff02c3fc830e64c768df234ea369fd599b31443
hayao <shun819.mail@gmail.com>
2020-05-18 12:02:39 +0900

    [update] : Added automatic start of snapd.

---
+++ channels/xfce/airootfs.x86_64/root/customize_airootfs_xfce.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+#
+# Yamada Hayao
+# Twitter: @Hayao0819
+# Email  : hayao@fascode.net
+#
+# (c) 2019-2020 Fascode Network.
+#
+
+set -e -u
+
+
+# Default value
+# All values can be changed by arguments.
+password=alter
+boot_splash=false
+kernel='zen'
+theme_name=alter-logo
+rebuild=false
+japanese=false
+username='alter'
+os_name="Alter Linux"
+install_dir="alter"
+usershell="/bin/bash"
+debug=true
+
+
+# Parse arguments
+while getopts 'p:bt:k:rxju:o:i:s:da:' arg; do
+    case "${arg}" in
+        p) password="${OPTARG}" ;;
+        b) boot_splash=true ;;
+        t) theme_name="${OPTARG}" ;;
+        k) kernel="${OPTARG}" ;;
+        r) rebuild=true ;;
+        j) japanese=true;;
+        u) username="${OPTARG}" ;;
+        o) os_name="${OPTARG}" ;;
+        i) install_dir="${OPTARG}" ;;
+        s) usershell="${OPTARG}" ;;
+        d) debug=true ;;
+        x) debug=true; set -xv ;;
+        a) arch="${OPTARG}"
+    esac
+done
+
+
+# Delete file only if file exists
+# remove <file1> <file2> ...
+function remove () {
+    local _list
+    local _file
+    _list=($(echo "$@"))
+    for _file in "${_list[@]}"; do
+        if [[ -f ${_file} ]]; then
+            rm -f "${_file}"
+        elif [[ -d ${_file} ]]; then
+            rm -rf "${_file}"
+        fi
+        echo "${_file} was deleted."
+    done
+}
+
+
+# Replace wallpaper.
+if [[ -f /usr/share/backgrounds/xfce/xfce-stripes.png ]]; then
+    remove /usr/share/backgrounds/xfce/xfce-stripes.png
+    ln -s /usr/share/backgrounds/alter.png /usr/share/backgrounds/xfce/xfce-stripes.png
+fi
+[[ -f /usr/share/backgrounds/alter.png ]] && chmod 644 /usr/share/backgrounds/alter.png
+
+
+# Bluetooth
+rfkill unblock all
+systemctl enable bluetooth
+
+# Snap
+systemctl enable snapd.apparmor.service
+systemctl enable apparmor.service
+systemctl enable snapd.socket
+systemctl enable snapd.service
+
+
+
+# Update system datebase
+dconf update
