660e3e8f533b1f9da1dbce025515dea89345cdb2
hayao <shun819.mail@gmail.com>
2020-05-26 10:41:25 +0900

    [update] : Added processing when Plymouth is disabled.

--- channels/lxde/airootfs.any/root/customize_airootfs_lxde.sh
+++ channels/lxde/airootfs.any/root/customize_airootfs_lxde.sh
@@ -116,11 +116,15 @@ usermod -aG autologin ${username}
 
 
 # Enable LightDM to auto login
-# Enable LightDM to auto login
 systemctl disable lightdm.service
-systemctl disable lightdm-plymouth.service
-systemctl enable live-lightdm.service
-systemctl enable live-lightdm-plymouth.service
+if [[ "${boot_splash}" = true ]]; then
+    systemctl disable lightdm-plymouth.service
+    systemctl enable live-lightdm.service
+    systemctl enable live-lightdm-plymouth.service
+else
+    remove "/usr/lib/systemd/system/live-lightdm.service"
+    remove "/usr/lib/systemd/system/live-lightdm-plymouth.service"
+fi
 
 
 # Replace auto login user
