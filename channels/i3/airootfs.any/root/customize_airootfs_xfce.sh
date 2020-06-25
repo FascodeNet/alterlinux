d4d18cca46e144b2f2ef787283696353276871b3
hayao <shun819.mail@gmail.com>
2020-06-25 21:25:19 +0900

    [disable] : Disabled sidebar script.

--- channels/i3/airootfs.any/root/customize_airootfs_xfce.sh
+++ channels/i3/airootfs.any/root/customize_airootfs_xfce.sh
@@ -118,7 +118,7 @@ fi
 
 
 # Set script permission
-chmod 755 /usr/local/bin/alterlinux-sidebar
+#chmod 755 /usr/local/bin/alterlinux-sidebar
 
 # Replace auto login user
 sed -i s/%USERNAME%/${username}/g /etc/lightdm/lightdm.conf
