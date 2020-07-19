29e24343183bb6570ec690ae876448e9451d610d
hayao <shun819.mail@gmail.com>
2020-07-19 13:02:46 +0900

    [change] : Changed gdm service

--- channels/gnome/airootfs.any/root/customize_airootfs_gnome.sh
+++ channels/gnome/airootfs.any/root/customize_airootfs_gnome.sh
@@ -116,16 +116,16 @@ else
 fi
 
 # Replace auto login user
-sed -i s/%USERNAME%/${username}/g /etc/lightdm/lightdm.conf
+sed -i s/%USERNAME%/${username}/g /etc/gdm/custom.conf
 
 # Added autologin group to auto login
 groupadd autologin
 usermod -aG autologin ${username}
 
 
-# Enable LightDM to auto login
+# Enable gdm to auto login
 if [[ "${boot_splash}" =  true ]]; then
-    systemctl enable lightdm-plymouth.service
+    systemctl enable gdm-plymouth.service
 else
-    systemctl enable lightdm.service
+    systemctl enable gdm.service
 fi
