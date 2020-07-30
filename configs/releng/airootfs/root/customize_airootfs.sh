0ada52d47f4bb98e8843663e8e2fa05dd52f58f4
nl6720 <nl6720@gmail.com>
2020-07-30 14:27:41 +0300

    releng: place custom sshd_config in airootfs
    Properly track the file instead of modifying it on-the-fly with customize_airootfs.sh.
    See https://gitlab.archlinux.org/archlinux/archiso/-/issues/21 .

--- configs/releng/airootfs/root/customize_airootfs.sh
+++ configs/releng/airootfs/root/customize_airootfs.sh
@@ -9,5 +9,4 @@ echo 'Warning: customize_airootfs.sh is deprecated! Support for it will be remov
 sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
 locale-gen
 
-sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
