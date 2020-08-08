ec513cdcd62d1c92bb0c6a24bbfec82fd00f7d14
hayao <shun819.mail@gmail.com>
2020-08-08 10:59:38 +0900

    [update] : install depends

--- system/aur_scripts/PKGBUILD_DEPENDS_SANDBOX.sh
+++ system/aur_scripts/PKGBUILD_DEPENDS_SANDBOX.sh
@@ -9,12 +9,12 @@
 #
 
 set -e -u 
-cd `dirname $0`
-if [ 1 -gt $# ];then
+cd "$(dirname $0)"
+if [[ 1 -gt $# ]];then
     echo "missing pkgbuild name"
     exit 1
 fi
-pkgbuild_data=`cat $1`
+pkgbuild_data=$(cat $1)
 eval ${pkgbuild_data}
 echo ${makedepends[@]}
-
+echo ${depends[@]}
