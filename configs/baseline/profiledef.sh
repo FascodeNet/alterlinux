7c2247f615ecdff8d236ac90c4bec1c3d255a497
nl6720 <nl6720@gmail.com>
2020-08-12 19:42:02 +0300

    archiso/mkarchiso: allow choosing boot modes from profiledef.sh
    Boot mode names are:
    
    - bios_syslinux.mbr: SYSLINUX in MBR
    - bios.syslinux.eltorito: SYSLINUX (ISOLINUX) via El Torito
    - uefi-x64.systemd-boot.esp: systemd-boot on ESP in MBR
    - uefi-x64.systemd-boot.eltorito: systemd-boot on ESP via El Torito
    
    It is not yet possible to create an ISO with only El Torito or only MBR boot modes!

--- configs/baseline/profiledef.sh
+++ configs/baseline/profiledef.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="archlinux-baseline"
 iso_label="ARCH_$(date +%Y%m)"
 iso_publisher="Arch Linux <https://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_application="Arch Linux baseline"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
