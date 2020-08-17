d01ff1ad175b4044c7bcd4d340f1d2c6d3867cfe
hayao <shun819.mail@gmail.com>
2020-08-17 12:33:56 +0900

    [update] : disable fcitx env if not jp

--- channels/gnome/airootfs.any/root/customize_airootfs_gnome.sh
+++ channels/gnome/airootfs.any/root/customize_airootfs_gnome.sh
@@ -123,3 +123,11 @@ chmod 755 /usr/bin/alterlinux-gtk-bookmarks
 
 # Replace auto login user
 sed -i s/%USERNAME%/${username}/g /etc/gdm/custom.conf
+
+
+# Remove file for japanese input
+if [[ ! "${language}" = "ja" ]]; then
+    sed -i "s/export GTK_IM_MODULE=fcitx/#export GTK_IM_MODULE=fcitx/g"
+    sed -i "s/export QT_IM_MODULE=fcitx/#export QT_IM_MODULE=fcitx/g" "/etc/environment"
+    sed -i "s/export XMODIFIERS=@im=fcitx/#export XMODIFIERS=@im=fcitx/g" "/etc/environment"
+fi
