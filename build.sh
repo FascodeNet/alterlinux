12dc064b2355f42203c52847622698fef41ef296
hayao <shun819.mail@gmail.com>
2020-08-24 14:56:21 +0900

    [update] : Increased information included in alteriso_info.

--- build.sh
+++ build.sh
@@ -1249,12 +1249,14 @@ make_prepare() {
             echo "${@}" >> "${_info_file}"
         }
         rm -rf "${_info_file}"; touch "${_info_file}"
+        _write_info_file "${iso_application} ${arch}"
         if [[ -d "${script_path}/.git" ]] && [[ "${gitversion}" = false ]]; then
-            _write_info_file "${iso_application} ${arch} ${iso_version}-$(git rev-parse --short HEAD)"
+            _write_info_file "       Version: ${iso_version}-$(git rev-parse --short HEAD)"
         else
-            _write_info_file "${iso_application} ${arch} ${iso_version}"
+            _write_info_file "       Version: ${iso_application} ${arch} ${iso_version}"
         fi
         _write_info_file "Created by ${iso_publisher}"
+        _write_info_file "Channel   name: ${channel_name}"
         _write_info_file "Live user name: ${username}"
         _write_info_file "Live user pass: ${password}"
     fi
