d646c3a8d6c0ca9d7570a300ac4937f1dbc418f0
hayao <shun819.mail@gmail.com>
2020-08-30 11:32:35 +0900

    [update] : alteriso3 services

--- channels/releng/airootfs.any/root/customize_airootfs.sh
+++ channels/releng/airootfs.any/root/customize_airootfs.sh
@@ -77,7 +77,7 @@ function remove () {
 remove /etc/skel/Desktop
 remove /root/Desktop
 
-if [[ ${arch} = "i686" ]]; then
+if [[ "${arch}" = "i686" ]]; then
     ln -s /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist32
 fi
 
@@ -91,13 +91,11 @@ cp -aT /etc/skel/ /root/
 
 sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
-sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
-sed -i 's/#\(HandleSuspendKey=\)suspend/\1ignore/' /etc/systemd/logind.conf
-sed -i 's/#\(HandleHibernateKey=\)hibernate/\1ignore/' /etc/systemd/logind.conf
-sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
-
-systemctl enable pacman-init.service choose-mirror.service
+# Enable services.
+systemctl enable pacman-init.service
+systemctl enable alteriso-reflector.service
+systemctl disable reflector.service
 systemctl set-default multi-user.target
 
 remove /etc/arch-release
