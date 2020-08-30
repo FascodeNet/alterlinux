a8f6b7bddaf13500a9b154ea7746994839ce1266
hayao <shun819.mail@gmail.com>
2020-08-30 11:55:53 +0900

    [fix] : dir copy

--- build.sh
+++ build.sh
@@ -1077,9 +1077,9 @@ make_syslinux() {
 
     # 一時ディレクトリに設定ファイルをコピー
     mkdir -p "${work_dir}/${arch}/syslinux/"
-    cp -aT "${script_path}/syslinux/${arch}/"* "$work_dir/${arch}/syslinux/"
+    cp -a "${script_path}/syslinux/${arch}/"* "$work_dir/${arch}/syslinux/"
     if [[ -d "${script_path}/channels/${channel_name}/syslinux.${arch}" ]] && [[ "${customized_syslinux}" = false ]]; then
-        cp -aT "${script_path}/channels/${channel_name}/syslinux.${arch}" "$work_dir/${arch}/"
+        cp -a "${script_path}/channels/${channel_name}/syslinux.${arch}" "$work_dir/${arch}/"
     fi
     
     # copy all syslinux config to work dir
