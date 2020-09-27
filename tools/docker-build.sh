dc403aaca99f05a994624fda5c010c779e4ddc28
yangniao23 <yangniao23@gmail.com>
2020-09-28 00:00:21 +0900

    UPDATE: docker対応を強化

--- tools/docker-build.sh
+++ tools/docker-build.sh
@@ -11,7 +11,45 @@ if ! type docker >/dev/null 2>&1; then
     exit 1
 fi
 
+_usage () {
+    echo "usage ${0} [options]"
+    echo
+    echo " General options:"
+    echo "    -o | --build-opiton \"[options]\"     send the build option to build.sh"
+    echo "    -c | --clean                          Enable --no-cache option when build docker image"
+    echo "    -h | --help                           This help message and exit"
+    echo
+}
+
+# Start parse options
+ARGUMENT="${@}"
+while (( $# > 0 )); do
+    case ${1} in
+        -o | --build-opiton)
+            BUILD_OPT="${2}"
+            shift 2
+            ;;
+        -c | --clean)
+            NO_CACHE="--no-cache" # Enable --no-cache option
+            shift 1
+            ;;
+        -h | --help)
+            _usage
+            exit 0
+            ;;
+        --)
+            shift
+            break
+            ;;
+        *)
+            msg_error "Invalid argument '${1}'"
+            _usage 1
+            ;;
+    esac
+done
+# End parse options
+
 script_path="$( cd -P "$( dirname "$(readlink -f "$0")" )" && pwd )/.."
-cd $SCRIPT_DIR
-docker build -t alterlinux-build:latest .
-docker run -e _DOCKER=true -t -i --privileged -v $SCRIPT_DIR/out:/alterlinux/out -v /usr/lib/modules:/usr/lib/modules:ro alterlinux-build
+cd $script_path
+docker build ${NO_CACHE} -t alterlinux-build:latest .
+docker run -e _DOCKER=true -t -i --privileged -v $script_path/out:/alterlinux/out -v /usr/lib/modules:/usr/lib/modules:ro alterlinux-build "${BUILD_OPT}"
