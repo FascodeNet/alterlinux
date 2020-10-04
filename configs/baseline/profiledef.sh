ae0e914114d2ff84e43b4da3b1b2bf855cff01d3
nl6720 <nl6720@gmail.com>
2020-10-04 17:48:15 +0300

    configs/baseline: support UEFI

--- configs/baseline/profiledef.sh
+++ configs/baseline/profiledef.sh
@@ -7,6 +7,6 @@ iso_publisher="Arch Linux <https://www.archlinux.org>"
 iso_application="Arch Linux baseline"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
