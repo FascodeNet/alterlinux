9b0d5614adb5bf0350dca015e50c2cc080cfacc7
hayao <shun819.mail@gmail.com>
2020-10-04 11:24:05 +0900

    [fix] : Fixed the script name of the message etc.

--- tools/fullbuild.sh
+++ tools/fullbuild.sh
@@ -30,26 +30,22 @@ all_channel=false
 # Show an INFO message
 # $1: message string
 msg_info() {
-    local _msg_opts="-a build.sh"
+    local _msg_opts="-a fullbuild.sh"
     if [[ "${1}" = "-n" ]]; then
         _msg_opts="${_msg_opts} -o -n"
         shift 1
     fi
-    [[ "${msgdebug}" = true ]] && _msg_opts="${_msg_opts} -x"
-    [[ "${nocolor}"  = true ]] && _msg_opts="${_msg_opts} -n"
     "${script_path}/tools/msg.sh" ${_msg_opts} info "${1}"
 }
 
 # Show an Warning message
 # $1: message string
 msg_warn() {
-    local _msg_opts="-a build.sh"
+    local _msg_opts="-a fullbuild.sh"
     if [[ "${1}" = "-n" ]]; then
         _msg_opts="${_msg_opts} -o -n"
         shift 1
     fi
-    [[ "${msgdebug}" = true ]] && _msg_opts="${_msg_opts} -x"
-    [[ "${nocolor}"  = true ]] && _msg_opts="${_msg_opts} -n"
     "${script_path}/tools/msg.sh" ${_msg_opts} warn "${1}"
 }
 
@@ -57,13 +53,11 @@ msg_warn() {
 # $1: message string
 msg_debug() {
     if [[ "${debug}" = true ]]; then
-        local _msg_opts="-a build.sh"
+        local _msg_opts="-a fullbuild.sh"
         if [[ "${1}" = "-n" ]]; then
             _msg_opts="${_msg_opts} -o -n"
             shift 1
         fi
-        [[ "${msgdebug}" = true ]] && _msg_opts="${_msg_opts} -x"
-        [[ "${nocolor}"  = true ]] && _msg_opts="${_msg_opts} -n"
         "${script_path}/tools/msg.sh" ${_msg_opts} info "${1}"
     fi
 }
@@ -72,13 +66,11 @@ msg_debug() {
 # $1: message string
 # $2: exit code number (with 0 does not exit)
 msg_error() {
-    local _msg_opts="-a build.sh"
+    local _msg_opts="-a fullbuild.sh"
     if [[ "${1}" = "-n" ]]; then
         _msg_opts="${_msg_opts} -o -n"
         shift 1
     fi
-    [[ "${msgdebug}" = true ]] && _msg_opts="${_msg_opts} -x"
-    [[ "${nocolor}"  = true ]] && _msg_opts="${_msg_opts} -n"
     "${script_path}/tools/msg.sh" ${_msg_opts} error "${1}"
     if [[ -n "${2:-}" ]]; then
         exit ${2}
