866d63427559c94d63c33f3d1a44e7dddcba51e5
hayao <shun819.mail@gmail.com>
2020-10-04 11:45:34 +0900

    [update] : Use clean.sh instead of rm

--- build.sh
+++ build.sh
@@ -374,7 +374,7 @@ prepare_env() {
     _trap_remove_work() {
         local status=${?}
         echo
-        remove "${work_dir}"
+        "${script_path}/tools/clean.sh" -o -w $(realpath "${work_dir}") $([[ "${debug}" = true ]] && echo -n "-d")
         exit ${status}
     }
     trap '_trap_remove_work' 1 2 3 15
@@ -1517,6 +1517,6 @@ run_once make_efiboot
 [[ "${tarball}" = true ]] && run_once make_tarball
 
 [[ "${noiso}" = false ]] && run_once make_iso
-[[ "${cleaning}" = true ]] && remove "${work_dir}"
+[[ "${cleaning}" = true ]] && "${script_path}/tools/clean.sh" -o -w $(realpath "${work_dir}") $([[ "${debug}" = true ]] && echo -n "-d")
 
 exit 0
