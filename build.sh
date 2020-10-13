46302b8759ca1e8ddfcf74f09062ed2024235072
hayao <shun819.mail@gmail.com>
2020-10-13 23:24:35 +0900

    [fix] : boot/ in efiboot.img

--- build.sh
+++ build.sh
@@ -1135,7 +1135,7 @@ make_efiboot() {
     mkdir -p "${work_dir}/efiboot/EFI/boot"
     (
         local __bootfile="$(basename "$(ls "${work_dir}/${arch}/airootfs/usr/lib/systemd/boot/efi/systemd-boot"*".efi" )")"
-        cp "${work_dir}/${arch}/airootfs/usr/lib/systemd/boot/efi/${__bootfile}" "${work_dir}/iso/EFI/boot/${__bootfile#systemd-}"
+        cp "${work_dir}/${arch}/airootfs/usr/lib/systemd/boot/efi/${__bootfile}" "${work_dir}/efiboot/boot/${__bootfile#systemd-}"
     )
 
     mkdir -p "${work_dir}/efiboot/loader/entries"
