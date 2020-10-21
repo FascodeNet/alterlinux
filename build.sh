1e586ca31337f170376247571c84cd9f6ed1a9f0
hayao <shun819.mail@gmail.com>
2020-10-21 15:12:49 +0900

    [fix] : efiboot.img create dir

--- build.sh
+++ build.sh
@@ -1123,10 +1123,8 @@ make_efiboot() {
     truncate -s 128M "${isofs_dir}/EFI/alteriso/efiboot.img"
     mkfs.fat -n ARCHISO_EFI "${isofs_dir}/EFI/alteriso/efiboot.img"
 
-    mkdir -p "${work_dir}/efiboot"
+    mkdir -p "${work_dir}/efiboot/EFI/alteriso/${arch}"
     mount "${isofs_dir}/EFI/alteriso/efiboot.img" "${work_dir}/efiboot"
-
-    mkdir -p "${work_dir}/efiboot/EFI/alteriso"
 
     cp "${isofs_dir}/${install_dir}/boot/${arch}/${kernel_filename}" "${work_dir}/efiboot/EFI/alteriso/${arch}/${kernel_filename}.efi"
     cp "${isofs_dir}/${install_dir}/boot/${arch}/archiso.img" "${work_dir}/efiboot/EFI/alteriso/${arch}/archiso.img"
