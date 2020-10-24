551edcd56dd02533505a6a671045a5a00bbdc42c
Alexander Epaneshnikov <aarnaarn2@gmail.com>
2020-10-25 00:11:22 +0300

    run_archiso: fixup !86
    forgot to enable USB support.

--- scripts/run_archiso.sh
+++ scripts/run_archiso.sh
@@ -92,7 +92,7 @@ run_image() {
         -device hda-output,audiodev=snd0 \
         -device virtio-net-pci,romfile=,netdev=net0 -netdev user,id=net0 \
         -device usb-braille,id=usbbrl,chardev=brltty \
-        -machine type=q35,smm=on,accel=kvm,pcspk-audiodev=snd0 \
+        -machine type=q35,smm=on,accel=kvm,usb=on,pcspk-audiodev=snd0 \
         -global ICH9-LPC.disable_s3=1 \
         -enable-kvm \
         "${qemu_options[@]}" \
