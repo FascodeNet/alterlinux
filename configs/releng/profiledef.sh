bb91fd52d9b2c2e8dc85ddefd5a2227b6eb309ba
Sven-Hendrik Haase <svenstaro@gmail.com>
2020-11-03 21:12:38 +0100

    Use better xz options
    Default: 732M
    With these options: 675M

--- configs/releng/profiledef.sh
+++ configs/releng/profiledef.sh
@@ -10,3 +10,4 @@ install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
