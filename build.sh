4c3eea7a8568884268cfe1a5b09e0cde5838b915
hayao <shun819.mail@gmail.com>
2020-11-13 21:41:22 +0900

    [revert] "[disable] : copy pacman.conf"
    This reverts commit bb99d6135d65384996cdd0fcf2261f8ab19ddd15.

--- build.sh
+++ build.sh
@@ -685,12 +685,12 @@ make_packages_aur() {
     done
     cp -rf "/etc/pacman.d/gnupg/" "${airootfs_dir}/etc/pacman.d/gnupg/"
     #cp -f "/etc/resolv.conf" "${airootfs_dir}/etc/resolv.conf"
-    #cp -f "${work_dir}/pacman-${arch}.conf" "${airootfs_dir}/etc/pacman.conf"
+    cp -f "${work_dir}/pacman-${arch}.conf" "${airootfs_dir}/etc/pacman.conf"
     # Create user to build AUR
     ${mkalteriso} ${mkalteriso_option} -w "${work_dir}/${arch}"  -D "${install_dir}" -r "/root/aur_prepare.sh ${pkglist_aur[*]}" run
     rm -rf "${airootfs_dir}/etc/pacman.d/gnupg/"
     #rm -rf "${airootfs_dir}/etc/resolv.conf"
-    #rm -rf "${airootfs_dir}/etc/pacman.conf"
+    rm -rf "${airootfs_dir}/etc/pacman.conf"
     # Remove the user created for the build.
     ${mkalteriso} ${mkalteriso_option} -w "${work_dir}/${arch}"  -D "${install_dir}" -r "/root/aur_remove.sh" run
 
