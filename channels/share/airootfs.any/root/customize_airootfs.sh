8263f2dc477711d39b5fee75b819995d8608ce67
hayao <shun819.mail@gmail.com>
2020-11-19 21:54:35 +0900

    [fix] : Quote string

--- channels/share/airootfs.any/root/customize_airootfs.sh
+++ channels/share/airootfs.any/root/customize_airootfs.sh
@@ -103,7 +103,9 @@ ln -sf /usr/share/zoneinfo/${timezone} /etc/localtime
 sed -i s/%OS_NAME%/"${os_name}"/g /etc/skel/Desktop/calamares.desktop
 
 # Create Calamares Entry
-cp /etc/skel/Desktop/calamares.desktop usr/share/applications/calamares.desktop
+if [[ -f "/etc/skel/Desktop/calamares.desktop" ]]; then
+    cp -a "/etc/skel/Desktop/calamares.desktop" "/usr/share/applications/calamares.desktop"
+fi
 
 
 # Creating a root user.
@@ -195,19 +197,19 @@ if [[ $boot_splash = true ]]; then
     # Edit calamares settings for Plymouth.
 
     # Use lightdm-plymouth instead of lightdm.
-    remove /usr/share/calamares/modules/services.conf
-    mv /usr/share/calamares/modules/services-plymouth.conf /usr/share/calamares/modules/services.conf
+    remove "/usr/share/calamares/modules/services.conf"
+    mv "/usr/share/calamares/modules/services-plymouth.conf" "/usr/share/calamares/modules/services.conf"
 
     # Back up default plymouth settings.
     # cp /usr/share/calamares/modules/plymouthcfg.conf /usr/share/calamares/modules/plymouthcfg.conf.org
 
     # Override theme settings.
-    remove /usr/share/calamares/modules/plymouthcfg.conf
-    echo '---' > /usr/share/calamares/modules/plymouthcfg.conf
-    echo "plymouth_theme: ${theme_name}" >> /usr/share/calamares/modules/plymouthcfg.conf
+    remove "/usr/share/calamares/modules/plymouthcfg.conf"
+    echo '---' > "/usr/share/calamares/modules/plymouthcfg.conf"
+    echo "plymouth_theme: ${theme_name}" >> "/usr/share/calamares/modules/plymouthcfg.conf"
 
     # Override plymouth settings.
-    sed -i s/%PLYMOUTH_THEME%/"${theme_name}"/g /etc/plymouth/plymouthd.conf
+    sed -i "s/%PLYMOUTH_THEME%/${theme_name}/g" "/etc/plymouth/plymouthd.conf"
 
     # Apply plymouth theme settings.
     plymouth-set-default-theme ${theme_name}
@@ -240,27 +242,27 @@ sed -i "s/%MKINITCPIO_PROFILE%/${kernel_mkinitcpio_profile}/g" /usr/share/calama
 sed -i "s|%KERNEL_FILENAME%|${kernel_filename}|g" /usr/share/calamares/modules/unpackfs.conf
 
 # Remove configuration files for other kernels.
-remove /usr/share/calamares/modules/initcpio/
-remove /usr/share/calamares/modules/unpackfs/
+remove "/usr/share/calamares/modules/initcpio/"
+remove "/usr/share/calamares/modules/unpackfs/"
 
 # Set up calamares removeuser
-sed -i s/%USERNAME%/${username}/g /usr/share/calamares/modules/removeuser.conf
+sed -i "s/%USERNAME%/${username}/g" "/usr/share/calamares/modules/removeuser.conf"
 
 # Set user shell
-sed -i "s|%USERSHELL%|'${usershell}'|g" /usr/share/calamares/modules/users.conf
+sed -i "s|%USERSHELL%|${usershell}|g" "/usr/share/calamares/modules/users.conf"
 
 # Set INSTALL_DIR
-sed -i s/%INSTALL_DIR%/"${install_dir}"/g /usr/share/calamares/modules/unpackfs.conf
+sed -i "s/%INSTALL_DIR%/${install_dir}/g" "/usr/share/calamares/modules/unpackfs.conf"
 
 # Set ARCH
-sed -i s/%ARCH%/"${arch}"/g /usr/share/calamares/modules/unpackfs.conf
+sed -i "s/%ARCH%/${arch}/g" "/usr/share/calamares/modules/unpackfs.conf"
 
 # Add disabling of sudo setting
-echo -e "\nremove \"/etc/sudoers.d/alterlive\"" >> /usr/share/calamares/final-process
+echo -e "\nremove \"/etc/sudoers.d/alterlive\"" >> "/usr/share/calamares/final-process"
 
 
 # Set os name
-sed -i s/%OS_NAME%/"${os_name}"/g /usr/lib/os-release
+sed -i "s/%OS_NAME%/${os_name}/g" "/usr/lib/os-release"
 
 
 # Enable root login with SSH.
@@ -269,7 +271,7 @@ if [[ -f "/etc/ssh/sshd_config" ]]; then
 fi
 
 # Un comment the mirror list.
-sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+sed -i "s/#Server/Server/g" "/etc/pacman.d/mirrorlist"
 
 # Set the os name to grub
 grub_os_name="${os_name%' Linux'}"
