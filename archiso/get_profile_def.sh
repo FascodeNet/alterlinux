550943bead8ab7b4669a0a7825771195021ecbcb
kokkiemouse <kokkiemouse@fascode.net>
2020-11-24 07:11:31 +0900

    add json parser
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- archiso/get_profile_def.sh
+++ archiso/get_profile_def.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 PROFILE_FILE=""
 SOURCE_FILE=""
+OUT_FILE=""
 usage_exit() {
-        echo "Usage: $0 -p [PROFILEDEF PATH] -s [SOURCE FILE NAME]" 1>&2
+        echo "Usage: $0 -p [PROFILEDEF PATH] -s [SOURCE FILE NAME] -o [OUT FILE]" 1>&2
         exit 1
 }
 
-while getopts p:s:h OPT
+while getopts p:s:o:h OPT
 do
     case $OPT in
         p)  PROFILE_FILE=$OPTARG
             ;;
         s)  SOURCE_FILE=$OPTARG
+            ;;
+        o)  OUT_FILE=$OPTARG
             ;;
         h)  usage_exit
             ;;
@@ -30,4 +33,4 @@ done
 bootmodes_js=$(echo ${bootmodes_js} | sed "s/stub,//g")
 cat ${SOURCE_FILE} | sed "s|ISONAME|${iso_name}|g" | sed "s|ISOLABEL|${iso_label}|g" | sed "s|ISOPUB|${iso_publisher}|g" | \
 sed "s|ISOAPP|${iso_application}|g" | sed "s|ISOVER|${iso_version}|g" | sed "s|INSTALLDIR|${install_dir}|g" | sed "s|ARCH|${arch}|g" \
-| sed "s|PACMANCONF|${pacman_conf}|g" | sed "s|\"BOOTMODES\"|${bootmodes_js}|g"
+| sed "s|PACMANCONF|${pacman_conf}|g" | sed "s|\"BOOTMODES\"|${bootmodes_js}|g" > ${OUT_FILE}
