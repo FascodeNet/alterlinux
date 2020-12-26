a178ff44bc9cf00918bb23b1e5f0a0c2395f47e9
nl6720 <nl6720@gmail.com>
2020-12-26 20:11:06 +0200

    profiledef.sh: Remove www. from archlinux.org
    See https://lists.archlinux.org/pipermail/arch-devops/2020-December/000474.html .

--- configs/baseline/profiledef.sh
+++ configs/baseline/profiledef.sh
@@ -3,7 +3,7 @@
 
 iso_name="archlinux-baseline"
 iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://www.archlinux.org>"
+iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux baseline"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
