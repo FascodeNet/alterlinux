1e7154fd9fa606211a0b2ecdbda05a6254536f9d
kokkiemouse <kokkiemouse@fascode.net>
2021-01-05 11:19:56 +0900

    fixed noshare
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- channels/releng/profiledef.sh
+++ channels/releng/profiledef.sh
@@ -9,6 +9,7 @@ iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
+noshare="true"
 pacman_conf="pacman.conf"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
