009c4681563322bd853fb10a628cff2f2b50d1af
hayao <hayao@fascode.net>
2021-01-17 15:54:04 +0900

    [remove] : Removed getopt(cinnamon)

--- channels/cinnamon/airootfs.any/root/customize_airootfs_cinnamon.sh
+++ channels/cinnamon/airootfs.any/root/customize_airootfs_cinnamon.sh
@@ -7,53 +7,6 @@
 # (c) 2019-2021 Fascode Network.
 #
 
-set -e -u
-
-
-# Default value
-# All values can be changed by arguments.
-password=alter
-boot_splash=false
-kernel_config_line=("zen" "vmlinuz-linux-zen" "linux-zen")
-theme_name=alter-logo
-rebuild=false
-username='alter'
-os_name="Alter Linux"
-install_dir="alter"
-usershell="/bin/bash"
-debug=false
-timezone="UTC"
-localegen="en_US\\.UTF-8\\"
-language="en"
-
-
-# Parse arguments
-while getopts 'p:bt:k:rxu:o:i:s:da:g:z:l:' arg; do
-    case "${arg}" in
-        p) password="${OPTARG}" ;;
-        b) boot_splash=true ;;
-        t) theme_name="${OPTARG}" ;;
-        k) kernel_config_line=(${OPTARG}) ;;
-        r) rebuild=true ;;
-        u) username="${OPTARG}" ;;
-        o) os_name="${OPTARG}" ;;
-        i) install_dir="${OPTARG}" ;;
-        s) usershell="${OPTARG}" ;;
-        d) debug=true ;;
-        x) debug=true; set -xv ;;
-        a) arch="${OPTARG}" ;;
-        g) localegen="${OPTARG/./\\.}\\" ;;
-        z) timezone="${OPTARG}" ;;
-        l) language="${OPTARG}" ;;
-    esac
-done
-
-
-# Parse kernel
-kernel="${kernel_config_line[0]}"
-kernel_filename="${kernel_config_line[1]}"
-kernel_mkinitcpio_profile="${kernel_config_line[2]}"
-
 # Enable LightDM to auto login
 if [[ "${boot_splash}" =  true ]]; then
     systemctl enable lightdm-plymouth.service
@@ -69,4 +22,3 @@ sed -i s/%PASSWORD%/${password}/g "/etc/dconf/db/local.d/02-disable-lock"
 
 # Update system datebase
 dconf update
-
