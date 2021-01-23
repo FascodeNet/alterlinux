a4ba5dcf1c09ed9002e51ffc798de34bea0ae4c2
kokkiemouse <kokkiemouse@fascode.net>
2021-01-23 11:40:45 +0900

    fixed
    Signed-off-by: kokkiemouse <kokkiemouse@fascode.net>

--- channels/releng/profiledef.sh
+++ channels/releng/profiledef.sh
@@ -10,6 +10,7 @@ install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
+noshare="true"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
