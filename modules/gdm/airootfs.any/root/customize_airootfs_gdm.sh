e83e24aea37b4128aa1990bd2baffad5974696a1
hayao <hayao@fascode.net>
2021-03-07 00:17:29 +0900

    [fix] : FIxed dm modules session

--- modules/gdm/airootfs.any/root/customize_airootfs_gdm.sh
+++ modules/gdm/airootfs.any/root/customize_airootfs_gdm.sh
@@ -19,31 +19,9 @@ fi
 sed -i "s/%USERNAME%/${username}/g" "/etc/gdm/custom.conf"
 
 
-# Set autologin session
-mkdir -p "/var/lib/AccountsService/users/"
-remove "/var/lib/AccountsService/users/${username}"
-cat > "/var/lib/AccountsService/users/${username}" << "EOF"
-[User]
-Language=
-Session=gnome-xorg
-XSession=gnome-xorg
-Icon=/home/${username}/.face
-SystemAccount=false
-EOF
-
-
 # Remove file for japanese input
 if [[ ! "${language}" = "ja" ]]; then
     sed -i "s/export GTK_IM_MODULE=fcitx/#export GTK_IM_MODULE=fcitx/g" "/etc/environment"
     sed -i "s/export QT_IM_MODULE=fcitx/#export QT_IM_MODULE=fcitx/g" "/etc/environment"
     sed -i "s/export XMODIFIERS=@im=fcitx/#export XMODIFIERS=@im=fcitx/g" "/etc/environment"
 fi
-
-
-# Remove shortcuts
-function remove_userfile() {
-    remove "/home/${username}/${1#/}"
-    remove "/etc/skel/${1#/}"
-}
-remove_userfile "Desktop/calamares.desktop"
-#remove_userfile ".config/autostart/genicon.desktop"
