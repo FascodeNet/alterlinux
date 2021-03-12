aae4f885dbe86cc25035a580ba8f10ac2427cc03
hayao <hayao@fascode.net>
2021-03-12 19:19:06 +0900

    [update] : Added transcate command

--- build.sh
+++ build.sh
@@ -598,6 +598,7 @@ make_pacman_conf() {
 # Base installation (airootfs)
 make_basefs() {
     msg_info "Creating ext4 image of 32GiB..."
+    truncate -s 32G -- "${airootfs_dir}.img"
     mkfs.ext4 -O '^has_journal,^resize_inode' -E 'lazy_itable_init=0' -m 0 -F -- "${airootfs_dir}.img" 32G
     tune2fs -c "0" -i "0" "${airootfs_dir}.img"
     msg_info "Done!"
