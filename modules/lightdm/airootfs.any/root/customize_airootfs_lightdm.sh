4ee731b44cbcf203cc0c7cfae191eebf02c74d37
hayao <hayao@fascode.net>
2021-03-20 16:11:40 +0900

    [add] : Session auto detect

--- modules/lightdm/airootfs.any/root/customize_airootfs_lightdm.sh
+++ modules/lightdm/airootfs.any/root/customize_airootfs_lightdm.sh
@@ -17,3 +17,21 @@ fi
 
 # Replace auto login user
 sed -i "s|%USERNAME%|${username}|g" "/etc/lightdm/lightdm.conf.d/02-autologin.conf"
+
+
+# Session list
+if cat "/etc/lightdm/lightdm.conf.d/02-autologin-session.conf" | grep "%SESSION%" 1> /dev/null 2>&1; then
+    session_list=()
+    while read -r session; do
+        session_list+=("${session}")
+    done < <(find "/usr/share/xsessions" -type f -print0 -name "*.desktop" | xargs -0 -I{} bash -c 'basename {} | sed "s|.desktop||g"')
+
+    if (( "${#session_list[@]}" = 1)); then
+        session="${session_list[*]}"
+        sed -i "s|%SESSION%|${session}|g" "/etc/lightdm/lightdm.conf.d/02-autologin-session.conf"
+    elif (( "${#session_list[@]}" = 0)); then
+        echo "Warining: Auto login session was not found"
+    else
+        remove "/etc/lightdm/lightdm.conf.d/02-autologin-session.conf"
+    fi
+fi
