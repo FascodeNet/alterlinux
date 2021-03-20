9bd45c8dea02bf9ec31f12ad5c958f49f7dd9270
hayao <hayao@fascode.net>
2021-03-20 16:27:19 +0900

    [fix] : Fixed the order to copy airootfs

--- build.sh
+++ build.sh
@@ -620,8 +620,8 @@ make_customize_airootfs() {
     # Overwrite airootfs with customize_airootfs.
     local _airootfs _airootfs_script_options _script _script_list _airootfs_list _main_script
 
-    _airootfs_list=("${channel_dir}/airootfs.${arch}" "${channel_dir}/airootfs.any")
-    for_module '_airootfs_list+=("${module_dir}/{}/airootfs.${arch}" "${module_dir}/{}/airootfs.any")'
+    _airootfs_list=("${channel_dir}/airootfs.any" "${channel_dir}/airootfs.${arch}")
+    for_module '_airootfs_list=("${module_dir}/{}/airootfs.any" "${module_dir}/{}/airootfs.${arch}" ${_airootfs_list[@]})'
 
     for _airootfs in ${_airootfs_list[@]};do
         if [[ -d "${_airootfs}" ]]; then
