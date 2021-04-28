83f94952db05f20ada0127c5fab8ddf83b308686
hayao <hayao@fascode.net>
2021-04-29 00:30:43 +0900

    [fix] : Install packages as root

--- system/pkgbuild.sh
+++ system/pkgbuild.sh
@@ -82,7 +82,7 @@ if (( "${#pkgbuild_dirs[@]}" != 0 )); then
         fi
         run_user makepkg -fACcs --noconfirm --skippgpcheck
         for pkg in $(run_user makepkg -f --packagelist); do
-            run_user pacman -S --noconfirm --needed --config /etc/alteriso-pacman.conf "${pkg}"
+            pacman -S --noconfirm --needed --config /etc/alteriso-pacman.conf "${pkg}"
         done
         cd - >/dev/null
     done
