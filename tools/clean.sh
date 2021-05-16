4044b1798f1293bb4769a8dd012b7318889c509c
hayao <hayao@fascode.net>
2021-05-16 21:47:11 +0900

    [update] : Use umount.sh

--- tools/clean.sh
+++ tools/clean.sh
@@ -4,6 +4,7 @@ set -eu
 
 script_path="$( cd -P "$( dirname "$(readlink -f "$0")" )" && cd .. && pwd )"
 work_dir="${script_path}/work"
+tools_dir="${script_path}/tools"
 debug=false
 only_work=false
 noconfirm=false
@@ -90,22 +91,7 @@ _umount() { if mountpoint -q "${1}"; then umount -lf "${1}"; fi; }
 
 # Unmount chroot dir
 umount_chroot () {
-    local _mount
-    if [[ ! -v "work_dir" ]] || [[ "${work_dir}" = "" ]]; then
-        msg_error "Exception error about working directory" 1
-    fi
-    if [[ ! -d "${work_dir}" ]]; then
-        return 0
-    fi
-    for _mount in $(find "${work_dir}" -mindepth 1 -type d -printf "%p\0" | xargs -0 -I{} bash -c "mountpoint -q {} && echo {}" | tac); do
-        if echo "${_mount}" | grep "${work_dir}" > /dev/null 2>&1 || echo "${_mount}" | grep "${script_path}" > /dev/null 2>&1 || echo "${_mount}" | grep "${out_dir}" > /dev/null 2>&1; then
-            msg_info "Unmounting ${_mount}"
-            _umount "${_mount}" 2> /dev/null
-        else
-            msg_error "It is dangerous to unmount a directory that is not managed by the script."
-            msg_error "Path: ${_mount}"
-        fi
-    done
+    "${tools_dir}/umount.sh" "${work_dir}"
 }
 
 # Usage: getclm <number>
