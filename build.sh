393b1e2faa85490eff505ec42e3ea1d763dad1a6
hayao <hayao@fascode.net>
2021-07-20 08:52:39 +0900

    [disable] : Disabled package check

--- build.sh
+++ build.sh
@@ -554,21 +554,20 @@ make_basefs() {
 # Additional packages (airootfs)
 make_packages_repo() {
     msg_debug "pkglist.sh ${pkglist_args[*]}"
-    readarray -t _pkglist < <("${tools_dir}/pkglist.sh" "${pkglist_args[@]}")
 
-    #pacman -Sy --config "${build_pacman_conf}"
-    readarray -t repopkgs < <(pacman-conf -c "${build_pacman_conf}" -l | xargs -I{} pacman -Sql --config "${build_pacman_conf}" --color=never {} && pacman -Sg)
-
-    local _pkg
-    for _pkg in "${_pkglist[@]}"; do
-        msg_info "Checking ${_pkg}..."
-        if printf "%s\n" "${repopkgs[@]}" | grep -qx "${_pkg}"; then
-            _pkglist_install+=("${_pkg}")
-        else
-            msg_info "${_pkg} was not found. Install it with yay from AUR"
-            norepopkg+=("${_pkg}")
-        fi
-    done
+    # Package check
+    #readarray -t _pkglist < <("${tools_dir}/pkglist.sh" "${pkglist_args[@]}")
+    #readarray -t repopkgs < <(pacman-conf -c "${build_pacman_conf}" -l | xargs -I{} pacman -Sql --config "${build_pacman_conf}" --color=never {} && pacman -Sg)
+    #local _pkg
+    #for _pkg in "${_pkglist[@]}"; do
+    #    msg_info "Checking ${_pkg}..."
+    #    if printf "%s\n" "${repopkgs[@]}" | grep -qx "${_pkg}"; then
+    #        _pkglist_install+=("${_pkg}")
+    #    else
+    #        msg_info "${_pkg} was not found. Install it with yay from AUR"
+    #        norepopkg+=("${_pkg}")
+    #    fi
+    #done
 
     # Create a list of packages to be finally installed as packages.list directly under the working directory.
     echo -e "# The list of packages that is installed in live cd.\n#\n" > "${build_dir}/packages.list"
