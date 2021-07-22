d1e2bda74806d7549e48340995f00439deab273f
hayao <hayao@fascode.net>
2021-07-22 17:28:27 +0000

    [remove] : Removed junk files

--- modules/share-extra/airootfs.any/root/customize_airootfs_share-extra.sh
+++ modules/share-extra/airootfs.any/root/customize_airootfs_share-extra.sh
@@ -28,23 +28,6 @@ usermod -aG autologin ${username}
 _systemd_service enable systemd-timesyncd.service
 
 
-# Pipewire
-# Do not use _systemd_service because pipewire services are not system but user
-# Use flag "--user --global"
-# https://gitlab.freedesktop.org/pipewire/pipewire/-/issues/923
-for _service in "pipewire.service" "pipewire-pulse.service"; do
-    #if systemctl --user --global cat "${_service}" 1> /dev/null 2>&1; then
-    if (( "$(systemctl --user --global list-unit-files "${_service}" | wc -l)" > 3 )); then
-        systemctl --user --global enable "${_service}"
-    fi
-done
-
-# Enable bluetooth support for pipewire
-if [[ -f "/etc/pipewire/media-session.d/media-session.conf" ]]; then
-    sed -i "s|#bluez5|bluez5 |g" "/etc/pipewire/media-session.d/media-session.conf"
-fi
-
-
 # Update system datebase
 if type -p dconf 1>/dev/null 2>/dev/null; then
     dconf update
