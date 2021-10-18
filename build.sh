639a8b63f72bf56266a6d52bdb873d58b1817aea
hayao <hayao@fascode.net>
2021-10-18 21:32:53 +0900

    [update] : Use _channel_check_version for version check

--- build.sh
+++ build.sh
@@ -2064,7 +2064,7 @@ fi
 
 # Check channel version
 _msg_debug "channel path is ${channel_dir}"
-if [[ ! "$(bash "${tools_dir}/channel.sh" --version "${alteriso_version}" ver "${channel_name}" | cut -d "." -f 1)" = "$(echo "${alteriso_version}" | cut -d "." -f 1)" ]] && [[ "${nochkver}" = false ]]; then
+if ! _channel_check_version "${channel_dir}"; then
     _msg_error "This channel does not support Alter ISO 3."
     if [[ -d "${script_path}/.git" ]]; then
         _msg_error "Please run \"git checkout alteriso-2\"" "1"
