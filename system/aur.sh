5653155794ec7b53d57ceec524c9d6d42c3930b3
hayao <hayao@fascode.net>
2021-11-22 22:29:48 +0900

    [fix] : Do not install with makepkg

--- system/aur.sh
+++ system/aur.sh
@@ -114,10 +114,10 @@ if ! pacman -Qq "${aur_helper_package}" 1> /dev/null 2>&1; then
     # Build
     sudo -u "${aur_username}" git clone "https://aur.archlinux.org/${aur_helper_package}.git" "/tmp/${aur_helper_package}"
     cd "/tmp/${aur_helper_package}"
-    sudo -u "${aur_username}" makepkg --ignorearch --clean --cleanbuild --force --skippgpcheck --noconfirm --install --syncdeps
+    sudo -u "${aur_username}" makepkg --ignorearch --clean --cleanbuild --force --skippgpcheck --noconfirm --syncdeps
 
     # Install
-    for _pkg in $(sudo -u "${aur_username}" makepkg --packagelist); do
+    for _pkg in $(cd "/tmp/${aur_helper_package}"; sudo -u "${aur_username}" makepkg --packagelist); do
         pacman "${pacman_args[@]}" -U "${_pkg}"
     done
 
