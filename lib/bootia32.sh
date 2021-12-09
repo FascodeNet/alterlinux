916060d47abafb9496af67d4a1cf1b26018436b6
hayao <hayao@fascode.net>
2021-12-09 21:03:36 +0900

    [fix] : Fixed awk for getting config from efi entry

--- lib/bootia32.sh
+++ lib/bootia32.sh
@@ -32,7 +32,7 @@ _make_bootmode_uefi-ia32.grub(){
 
     # _get_efiboot_entry <path> <key>
     _get_efiboot_entry(){
-        awk "BEGIN {
+        awk "{
             if (\$1 == \"${2}\"){
                 print \$2
             }
