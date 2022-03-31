dd4ffaf67e56ccebc54eb65d73424ccb30bfe60d
hayao <hayao@fascode.net>
2022-03-31 22:49:00 +0900

    [update] : Add linux-firmware-marvell
    See https://github.com/archlinux/archiso/commit/8b1788e1bf54022608bb7bd9e72aea1a8b3bf286

--- modules/share/airootfs.any/root/.automated_script.sh
+++ modules/share/airootfs.any/root/.automated_script.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-#
-# Yamada Hayao
-# Twitter: @Hayao0819
-# Email  : hayao@fascode.net
-#
-# (c) 2019-2021 Fascode Network.
-#
 
 script_cmdline ()
 {
@@ -23,7 +16,7 @@ automated_script ()
     script="$(script_cmdline)"
     if [[ -n "${script}" && ! -x /tmp/startup_script ]]; then
         if [[ "${script}" =~ ^((http|https|ftp)://) ]]; then
-            curl "${script}" --retry-connrefused -s -o /tmp/startup_script >/dev/null
+            curl "${script}" --location --retry-connrefused --retry 10 -s -o /tmp/startup_script >/dev/null
             rt=$?
         else
             cp "${script}" /tmp/startup_script
@@ -36,6 +29,6 @@ automated_script ()
     fi
 }
 
-if [[ "$(tty)" = "/dev/tty1" ]]; then
+if [[ $(tty) == "/dev/tty1" ]]; then
     automated_script
 fi
