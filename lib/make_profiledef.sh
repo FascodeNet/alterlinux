587db99a1b3f90cb92f354b0d29098c4305457ab
hayao <hayao@fascode.net>
2023-04-18 19:35:19 +0900

    [update] : Support some variables

--- lib/make_profiledef.sh
+++ lib/make_profiledef.sh
@@ -5,7 +5,9 @@ make_profiledef(){
     local _base="${template_dir}/profiledef.sh" _args=()
     
 
-    parse_template "$_base" iso_name
+    parse_template "$_base" \
+        iso_name iso_label iso_publisher iso_application \
+        iso_version install_dir arch noiso noefi
 }
 
 
