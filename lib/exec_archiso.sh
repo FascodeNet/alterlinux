9ff7178c1a43adac112fcb63d5fbfabc27db6722
hayao <hayao@fascode.net>
2023-05-23 08:58:54 +0900

    [update] : Accept to pass args to archiso

--- lib/exec_archiso.sh
+++ lib/exec_archiso.sh
@@ -3,6 +3,6 @@
 
 exec_archiso(){
     git clone "https://github.com/FascodeNet/archiso-alter" "$work_dir/archiso-alter"
-    bash "$work_dir/archiso-alter/archiso/mkarchiso" -v -w "$work_dir/build" -o "$out_dir" "$work_dir/profile"
+    bash "$work_dir/archiso-alter/archiso/mkarchiso" -v -w "$work_dir/build" -o "$out_dir" "$@" "$work_dir/profile"
 
 }
