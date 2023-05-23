5a24da911d89ded052b4d1de21de884bcfd43d5d
hayao <hayao@fascode.net>
2023-05-23 09:05:28 +0900

    [fix] : Use submodule archiso

--- lib/exec_archiso.sh
+++ lib/exec_archiso.sh
@@ -2,7 +2,5 @@
 # shellcheck disable=SC2154
 
 exec_archiso(){
-    git clone "https://github.com/FascodeNet/archiso-alter" "$work_dir/archiso-alter"
-    bash "$work_dir/archiso-alter/archiso/mkarchiso" -v -w "$work_dir/build" -o "$out_dir" "$@" "$work_dir/profile"
-
+    bash "${script_path}/archiso/archiso/mkarchiso" -v -w "$work_dir/build" -o "$out_dir" "$@" "$work_dir/profile"
 }
