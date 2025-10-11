f230ec69150b2644f8bc60ae9d5fad89221678bb
hayao <shun819.mail@gmail.com>
2025-10-11 22:27:53 +0900

    feat: Add root check to clean.sh for elevated permissions

--- alteriso/clean.sh
+++ alteriso/clean.sh
@@ -4,6 +4,11 @@ set -eEuo pipefail
 
 script_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
+# Root check
+if [[ $EUID -ne 0 ]]; then
+    exec sudo "$0" "$@"
+fi
+
 # Unmount
 mount | cut -d ' ' -f 3 | grep "$script_path/work" | sort -r | xargs -r umount -lf || true
 
