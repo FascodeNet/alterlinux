5bcc2cb230a95b4a63d9b2ebcadc97ea1c1c926e
hayao <shun819.mail@gmail.com>
2025-10-12 05:37:36 +0900

    feat: Add user auto-login setup and refactor injecter script

--- alteriso/src/internal/archiso/injects/injecter.sh
+++ alteriso/src/internal/archiso/injects/injecter.sh
@@ -37,20 +37,3 @@ override__make_customize_airootfs() {
         _msg_info "Done! customize_airootfs.sh run successfully."
     fi
 }
-
-_make_passwd() {
-    local _username
-    _username=$(__alteriso_profiledef | jq -r ".username")
-
-    _msg_info "Setting up auto-login for user: $_username"
-
-    passwd+=("${_username}:x:1000:1000:Live User:/home/${_username}:/bin/zsh")
-    printf '%s\n' "${passwd[@]}" >> "${pacstrap_dir}/etc/passwd"
-    # shellcheck disable=SC2034
-    # file_permissions["/etc/passwd"]="0:0:644"
-    _msg_info "Setting up user for auto-login: $_username"
-}
-
-pre__make_customize_airootfs() {
-    _make_passwd
-}
