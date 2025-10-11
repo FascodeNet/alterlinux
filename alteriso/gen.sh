6467a282f0f1076e3773885854dd25be139c805b
hayao <shun819.mail@gmail.com>
2025-10-11 22:27:42 +0900

    feat: Run clean.sh before start building

--- alteriso/gen.sh
+++ alteriso/gen.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 script_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-go run "$script_path/src" profile generate \
+"$script_path/clean.sh"
+exec go run "$script_path/src" profile generate \
     --bootloaders "$script_path/bootloaders/"\
     --modules "$script_path/modules/" \
     -o "$script_path/out" "$@"
