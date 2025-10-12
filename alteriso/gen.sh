b779848efe700842ab82fdf1ffa8eb71de4f237f
hayao <shun819.mail@gmail.com>
2025-10-12 18:43:33 +0900

    fix: Correct command execution order in gen.sh for proper profile generation

--- alteriso/gen.sh
+++ alteriso/gen.sh
@@ -2,7 +2,8 @@
 
 script_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 "$script_path/clean.sh"
-exec go run "$script_path/src" profile generate \
-    --bootloaders "$script_path/bootloaders/"\
+exec go run "$script_path/src" profile \
+    --bootloaders "$script_path/bootloaders/" \
     --modules "$script_path/modules/" \
+    generate \
     -o "$script_path/out" "$@"
