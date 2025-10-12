42ceab6e63b568a9901b3d1621e275d28c150edc
hayao <shun819.mail@gmail.com>
2025-10-12 17:21:30 +0900

    fix: Fix module typo

--- alteriso/modules/base/module.sh
+++ alteriso/modules/base/module.sh
@@ -4,10 +4,10 @@
 __alteriso_base_setup_mkinitcpio_presets() {
     local _mkinitcpio_preset_file="$pacstrap_dir/etc/mkinitcpio.d/linux.preset.in"
     local _kernel_name
-    _kernel_name=$(__alteriso_profiledef_kernel_name)
+    _kernel_name=$(__alteriso_profiledef_kernelname)
 
     if [[ -e "${_mkinitcpio_preset_file}" ]]; then
-        sed "s|%ALTERISO_KERNEL_NAME%|${_kernel_name}|g" "$_mkinitcpio_preset_file" "$pacstrap_dir/etc/mkinitcpio.d/$__alteriso_profiledef_kernelname.preset"
+        sed "s|%ALTERISO_KERNEL_NAME%|${_kernel_name}|g" "$_mkinitcpio_preset_file" > "$pacstrap_dir/etc/mkinitcpio.d/$_kernel_name.preset"
         rm -f "$_mkinitcpio_preset_file"
     fi
 }
