08a8c4fbd03fc48c83773fb6d1e43677ffc59edb
hayao <shun819.mail@gmail.com>
2025-10-12 17:51:38 +0900

    fix: Remove trailing space in autologin configuration path

--- alteriso/modules/user/module.sh
+++ alteriso/modules/user/module.sh
@@ -10,7 +10,7 @@ _make_customize_airootfs_user() {
     passwd+=("${_username}:x:1000:1000:Live User:/home/${_username}:/bin/zsh")
     printf '%s\n' "${passwd[@]}" >>"${pacstrap_dir}/etc/passwd"
 
-    local _autologin_conf="$pacstrap_dir/etc/systemd/system/getty@tty1.service.d/autologin.conf "
+    local _autologin_conf="$pacstrap_dir/etc/systemd/system/getty@tty1.service.d/autologin.conf"
     if [[ -e "${_autologin_conf}" ]]; then
         sed -i "s|%ALTERISO_USERNAME%|${_username}|g" "${_autologin_conf}"
     fi
