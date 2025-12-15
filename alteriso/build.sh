cc8c5b967e4def8ecd2ee23b9b34fec799d3f01e
hayao <shun819.mail@gmail.com>
2025-12-15 11:25:12 +0900

    feat: Support specifying profile

--- alteriso/build.sh
+++ alteriso/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+set -Eeuo pipefail
 script_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 rm -rf "$script_path/out"
-"$script_path/gen.sh" "$script_path/configs/xfce" || exit $?
-
-sudo ../archiso/mkarchiso -v -w "$script_path/work" -o "$script_path/out" "$script_path/out/xfce"
+profile_dir="${1-""${script_path}/configs/xfce""}"
+"$script_path/gen.sh" "$profile_dir"
+sudo ../archiso/mkarchiso -v -w "$script_path/work" -o "$script_path/out" "$script_path/out/$(basename "$profile_dir")"
