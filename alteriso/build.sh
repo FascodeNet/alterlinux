fa1e190866635e0f86d57c5d34ce475ec0b0e649
hayao <shun819.mail@gmail.com>
2025-12-23 19:27:58 +0900

    refactor: Update build process and add legacy build script

--- alteriso/build.sh
+++ alteriso/build.sh
@@ -2,28 +2,31 @@
 
 set -Eeuo pipefail
 
+# Determine script path
 script_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 if [[ -n "${1-""}" ]]; then
     profile_dir="${1-"${script_path}/configs/xfce"}"
     shift 1
 fi
 work_dir="$script_path/work"
-cache_dir="$work_dir/alteriso_cache"
 
-mkdir -p "$work_dir/archiso"
-mkdir -p "$cache_dir"
+# Build the Go binary
+binfile="$(mktemp -u)"
+go build -o "$binfile" "$script_path/src"
 
-# Generate archiso profile
-"$script_path/gen.sh" -o "$work_dir/profile" "$profile_dir"
+# Set mkarchiso path
+PATH="$(realpath "$script_path/../archiso/"):$PATH"
+export PATH
 
-sudo ALTERISO_PACMAN_CACHE="$cache_dir" \
-    ../archiso/mkarchiso -v \
-    -w "$work_dir/archiso" \
-    -o "$script_path/out" \
-    "$work_dir/profile"
+# Execute the build command
+"$binfile" profile \
+    --bootloaders "$script_path/bootloaders/" \
+    --modules "$script_path/modules/" \
+    build \
+    --workdir "$work_dir" \
+    --outdir "$script_path/out" \
+    "$profile_dir" "$@"
 
-# sudo ../archiso/mkarchiso \
-#     -v \
-#     -w "$work_dir/archiso" \
-#     -o "$script_path/out" \
-#     "$work_dir/profile"
+# Clean up
+"$binfile" clean --workdir "$work_dir"
+rm  "$binfile"
