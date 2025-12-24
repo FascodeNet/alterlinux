44fc96963d9778f93036a89ae6b96ae3ea3a6137
hayao <shun819.mail@gmail.com>
2025-12-25 03:27:17 +0900

    remove: Remove legacy scripts

--- alteriso/gen.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-script_path=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-"$script_path/clean.sh"
-exec go run "$script_path/src" profile \
-    --bootloaders "$script_path/bootloaders/" \
-    --modules "$script_path/modules/" \
-    generate "$@"
