af9a17f907a3232212fb43782f41d0a37f2375f4
hayao <shun819.mail@gmail.com>
2026-01-15 21:41:03 +0900

    feat: Update install command to check for Arch Linux package and download mkarchiso script

--- alteriso/src/internal/cmd/install.go
+++ alteriso/src/internal/cmd/install.go
@@ -1,20 +1,194 @@
 package cmd
 
 import (
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/exec"
+
+	"github.com/Hayao0819/go-distro"
+	"github.com/Hayao0819/go-distro/linux"
+
 	"github.com/FascodeNet/alterlinux/src/internal/archiso"
+	"github.com/FascodeNet/alterlinux/src/internal/errors"
+	"github.com/Jguer/go-alpm/v2"
+	"github.com/go-git/go-git/v6"
+	"github.com/go-git/go-git/v6/plumbing"
 	"github.com/spf13/cobra"
 )
 
+func isPkgInstalled(name ...string) (bool, error) {
+
+	if !isArchLinux() {
+		return false, nil
+	}
+
+	h, err := alpm.Initialize("/", "/var/lib/pacman")
+	if err != nil {
+		return false, err
+	}
+	defer h.Release()
+
+	db, err := h.LocalDB()
+	if err != nil {
+		return false, err
+	}
+
+	pkg := db.Search(name)
+	if pkg == nil {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func downloadFile(url, dest string) error {
+	res, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	file, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, res.Body)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func checkoutBranch(repo *git.Repository, branch string) error {
+	w, err := repo.Worktree()
+	if err != nil {
+		return err
+	}
+
+	err = w.Checkout(&git.CheckoutOptions{
+		Branch: plumbing.ReferenceName(branch),
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func gitCloneIntoTempDir(repoURL string) (*git.Repository, string, error) {
+	tempDir, err := os.MkdirTemp("", "archiso-injectable-")
+	if err != nil {
+		return nil, "", err
+	}
+
+	// err = gitClone(repoURL, tempDir)
+	r, err := git.PlainClone(tempDir, &git.CloneOptions{
+		URL: repoURL,
+	})
+	if err != nil {
+		os.RemoveAll(tempDir)
+		return nil, "", err
+	}
+
+	return r, tempDir, nil
+}
+
+func isArchLinux() bool {
+	return distro.GetDetail().ID() == linux.Arch.ID()
+}
+
 func installArchisoCmd() *cobra.Command {
-	return &cobra.Command{
+	cmd := cobra.Command{
 		Use:   "install-archiso",
 		Short: "Install injectable Archiso",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			// return errors.New("not implemented yet")
-			archiso.IsInjectable()
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			installed, err := archiso.IsInjectable()
+			if err != nil {
+				return err
+			}
+			if installed {
+				return errors.New("archiso-injectable is already installed")
+			}
 			return nil
 		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			isArchisoInstalled, err := isPkgInstalled("archiso", "archiso-git")
+			if err != nil {
+				return err
+			}
+
+			githubOwner, err := cmd.Flags().GetString("github-owner")
+			if err != nil {
+				return err
+			}
+
+			githubRepo, err := cmd.Flags().GetString("github-repo")
+			if err != nil {
+				return err
+			}
+
+			githubRef, err := cmd.Flags().GetString("github-ref")
+			if err != nil {
+				return err
+			}
+
+			if isArchisoInstalled {
+				slog.Info("archiso package is installed, downloading mkarchiso script directly")
+				dest, err := cmd.Flags().GetString("script-dest")
+				if err != nil {
+					return err
+				}
+
+				// https://raw.githubusercontent.com/FascodeNet/alterlinux/refs/heads/dev/archiso/mkarchiso
+				url := fmt.Sprintf("https://raw.githubusercontent.com/%s/%s/%s/archiso/mkarchiso", githubOwner, githubRepo, githubRef)
+				err = downloadFile(url, dest)
+				if err != nil {
+					return err
+				}
+				err = os.Chmod(dest, 0755)
+				if err != nil {
+					return err
+				}
+				return nil
+			} else {
+				slog.Info("archiso package is not installed, cloning full repository and installing from source")
+				repoURL := fmt.Sprintf("https://github.com/%s/%s.git", githubOwner, githubRepo)
+				repo, tempDir, err := gitCloneIntoTempDir(repoURL)
+				if err != nil {
+					return err
+				}
+				defer os.RemoveAll(tempDir)
+
+				if err := checkoutBranch(repo, githubRef); err != nil {
+					return err
+				}
+
+				makeCmd := exec.Command("make", "install")
+				makeCmd.Dir = tempDir
+				makeCmd.Stdout = os.Stdout
+				makeCmd.Stderr = os.Stderr
+				if err := makeCmd.Run(); err != nil {
+					return err
+				}
+				return nil
+
+			}
+		},
 	}
+
+	cmd.Flags().StringP("script-dest", "", "/usr/local/bin/mkarchiso", "Local destination path for the mkarchiso script")
+	cmd.Flags().StringP("github-owner", "", "FascodeNet", "GitHub owner of the archiso-injectable repository")
+	cmd.Flags().StringP("github-repo", "", "archiso-injectable", "GitHub repository name of the archiso-injectable repository")
+	cmd.Flags().StringP("github-ref", "", "main", "GitHub reference (branch, tag, commit) of the archiso-injectable repository")
+
+	return &cmd
+
 }
 
 func init() {
