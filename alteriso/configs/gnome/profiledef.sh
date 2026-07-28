30eb81ed4c98c44773e5dd58d0a2f386e399a99e
hayao <shun819.mail@gmail.com>
2026-07-29 00:52:14 +0900

    refactor(alteriso): move the module-owned file permissions into modules

--- alteriso/configs/gnome/profiledef.sh
+++ alteriso/configs/gnome/profiledef.sh
@@ -15,13 +15,9 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
-    ["/etc/shadow"]="0:0:400"
     ["/root"]="0:0:750"
     ["/root/.automated_script.sh"]="0:0:755"
     ["/root/.gnupg"]="0:0:700"
-    ["/usr/local/bin/choose-mirror"]="0:0:755"
-    ["/usr/local/bin/Installation_guide"]="0:0:755"
-    ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
 
 git_revision=${GIT_REVISION-"$(git rev-parse --short HEAD 2>/dev/null || true)"}
